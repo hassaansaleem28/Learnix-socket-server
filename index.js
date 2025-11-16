f5fa97043cbbef768f32ce4d025b6b9a359f3292
M.Hassaan Saleem <167296971+hassaansaleem28@users.noreply.github.com>
2025-11-16 15:33:41 +0500

    Update CORS origin for production deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://learnix-lms.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
   }
